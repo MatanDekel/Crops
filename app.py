b19049334c4cda8a8fdb7ad72884c9f0449dafc5
MatanDekel <156198561+MatanDekel@users.noreply.github.com>
2024-08-10 13:50:00 +0300

    new

--- app.py
+++ app.py
@@ -28,10 +28,6 @@ def predict():
     })
 
 
-# except Exception as e:
-#     logging.error(f"Error in prediction: {e}")
-#     return jsonify({"error": str(e)}), 500
-
 
 if __name__ == "__main__":
     app.run(debug=True)
